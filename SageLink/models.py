3379dd9cf5708308cc68704714c028c42178587d
makayla-mcmullin <92887584+makayla-mcmullin@users.noreply.github.com>
2024-03-09 17:08:01 -0500

    new stuff 5:07

--- SageLink/models.py
+++ SageLink/models.py
@@ -19,12 +19,12 @@ class ElderUser(db.Model):
     password = db.Column(db.String(80), unique=True, nullable=False)
     location = db.Column(db.String(80), unique=True, nullable=False)
     
-    companionship = db.Column(db.TinyInt, unique=False, nullable=True)
-    grocery = db.Column(db.TinyInt, unique=False, nullable=True)
-    cleaning = db.Column(db.TinyInt, unique=False, nullable=True)
-    transportation = db.Column(db.TinyInt, unique=False, nullable=True)
+    companionship = db.Column(db.Boolean, unique=False, nullable=True)
+    grocery = db.Column(db.Boolean, unique=False, nullable=True)
+    cleaning = db.Column(db.Boolean, unique=False, nullable=True)
+    transportation = db.Column(db.Boolean, unique=False, nullable=True)
     
-    matches = db.relationship('matches', backref='ElderUser')
+    matches = db.relationship('Matches', backref='elder_user')
 
     
 
@@ -36,20 +36,20 @@ class HelperUser(db.Model):
    password = db.Column(db.String(80), unique=True, nullable=False)
    location = db.Column(db.String(80), unique=True, nullable=False)
    
-   companionship = db.Column(db.TinyInt, unique=False, nullable=True)
-   grocery = db.Column(db.TinyInt, unique=False, nullable=True)
-   cleaning = db.Column(db.TinyInt, unique=False, nullable=True)
-   transportation = db.Column(db.TinyInt, unique=False, nullable=True)
+   companionship = db.Column(db.Boolean, unique=False, nullable=True)
+   grocery = db.Column(db.Boolean, unique=False, nullable=True)
+   cleaning = db.Column(db.Boolean, unique=False, nullable=True)
+   transportation = db.Column(db.Boolean, unique=False, nullable=True)
    
-   matches = db.relationship('matches', backref='HelperUser')
+   matches = db.relationship('Matches', backref='helper_user')
 
    
 
 class Matches(db.Model):
     id = db.Column(db.Integer, primary_key=True)
 
-    helper_id = db.Column(db.Integer, db.ForeignKey('HelperUser.id'))
-    elder_id= db.Column(db.Integer, db.ForeignKey('ElderUser.id'))
+    helper_user_id = db.Column(db.Integer, db.ForeignKey('helper_user.id'))
+    elder_user_id= db.Column(db.Integer, db.ForeignKey('elder_user.id'))
 
 db.create_all()
 
